edf26ce9f8fcf3d57391ba949d910682f048a0f2
Prepodobnuy <prepodobnuy@inbox.ru>
2025-09-28 03:18:28 +0300

    Naming changes

--- lua/scratchim/buffer.lua
+++ lua/scratchim/buffer.lua
@@ -1,8 +1,8 @@
 local config = require 'scratchim.config'
 local debounce = require 'scratchim.debounce'
 local cache = require 'scratchim.cache'
+local augroup = vim.api.nvim_create_augroup('ScratchimChange', { clear = true })
 
---- Returns current buffer 'modified' option
 ---@return boolean
 local function is_buf_modified()
   local buf = vim.api.nvim_get_current_buf()
@@ -14,6 +14,7 @@ local function listen_changes()
 
   vim.api.nvim_create_autocmd({ 'TextChanged', 'TextChangedI', 'TextChangedP', 'TextChangedT' }, {
     buffer = buf,
+    group = augroup,
     callback = function()
       local lines = table.concat(vim.api.nvim_buf_get_lines(buf, 0, -1, false), '\n')
       cache.set(lines)
@@ -24,10 +25,10 @@ end
 
 ---@param buf integer
 ---@param capt string[]
-local function fill_buffer(buf, capt) vim.api.nvim_buf_set_lines(buf, 0, -1, false, capt) end
+local function buf_set_lines(buf, capt) vim.api.nvim_buf_set_lines(buf, 0, -1, false, capt) end
 
 ---@param buf integer
-local function set_cache(buf)
+local function buf_set_cache(buf)
   local lines = table.concat(vim.api.nvim_buf_get_lines(buf, 0, -1, false), '\n')
   cache.set(lines)
 end
@@ -38,7 +39,6 @@ local function get_cache()
   return vim.fn.split(cached_content, '\n', true)
 end
 
---- Opens existing scratch buffer
 ---@param split boolean?
 ---@param buf integer? buffer id
 local function open_existing_buffer(split, buf)
@@ -63,11 +63,10 @@ local function open_existing_buffer(split, buf)
 
   buf = vim.api.nvim_get_current_buf()
   local cached = get_cache()
-  fill_buffer(buf, cached)
+  buf_set_lines(buf, cached)
   listen_changes()
 end
 
---- Creates new scratch buffer
 ---@param split boolean?
 local function open_new_buffer(split)
   split = split or false
@@ -78,15 +77,15 @@ local function open_new_buffer(split)
   local buf = vim.api.nvim_get_current_buf()
 
   -- stylua: ignore start
-  -- vim.api.nvim_set_option_value('hide',      true,  { buf = buf }) -- what to do when the buffer is hidden
+  vim.api.nvim_set_option_value('bufhidden', 'hide',   { buf = buf }) -- what to do when the buffer is hidden
   vim.api.nvim_set_option_value('buflisted', true,     { buf = buf }) -- include the buffer in the :bnext list
   vim.api.nvim_set_option_value('buftype',   'nofile', { buf = buf }) -- nofile means the buffer isn't backed by a file and we control its name
   vim.api.nvim_set_option_value('swapfile',  false,    { buf = buf }) -- never swapfiles
   -- stylua: ignore end
 
   local cached = get_cache()
-  fill_buffer(buf, cached)
-  set_cache(buf)
+  buf_set_lines(buf, cached)
+  buf_set_cache(buf)
   listen_changes()
 end
 
